e353309c97797908a5d3b3d62d5822d78dd5003c
Alex Szabó <kreatemore@gmail.com>
2021-01-05 18:34:12 +0100

    Adds Action enum

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
 use std::env;
+use std::fmt::{Display, Formatter, Result};
 use std::fs;
 use std::fs::OpenOptions;
 use std::process::exit;
 
 mod key_utils;
 mod encryption;
+
+enum Action {
+    Encrypt,
+    Decrypt,
+}
+
+impl Display for Action {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        match *self {
+            Action::Encrypt => write!(f, "Encrypt"),
+            Action::Decrypt => write!(f, "Decrypt"),
+        }
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -23,19 +38,19 @@ fn main() {
                 eprintln!("Please provide a valid 32-long char-utf8 as a custom key");
                 exit_with_help();
             }
-            perform_action_on_directory(&encryption_key, action, working_directory);
+            perform_action_on_directory(&encryption_key, &Action::Encrypt, working_directory);
             println!("\n\n🔑 {}", encryption_key);
         }
         "decrypt" => {
             let encryption_key = key_utils::set_encryption_key(&args);
-            perform_action_on_directory(&encryption_key, action, working_directory);
+            perform_action_on_directory(&encryption_key, &Action::Decrypt, working_directory);
             println!("Done decrypting chief");
         }
         _ => exit_with_help(),
     }
 }
 
-fn perform_action_on_directory(encryption_key: &String, action: &str, working_directory: &str) {
+fn perform_action_on_directory(encryption_key: &String, action: &Action, working_directory: &str) {
     let directory_entries = fs::read_dir(working_directory).expect(working_directory);
 
     println!("Command: {} {}", action, working_directory);
@@ -53,9 +68,8 @@ fn perform_action_on_directory(encryption_key: &String, action: &str, working_di
             println!("{}::{}...", action, dir_entry.file_name().into_string().unwrap());
 
             match action {
-                "encrypt" => encryption::encrypt_file(&mut file, &encryption_key),
-                "decrypt" => encryption::decrypt_file(&mut file, &encryption_key),
-                _ => exit_with_help(),
+                Action::Encrypt => encryption::encrypt_file(&mut file, &encryption_key),
+                Action::Decrypt => encryption::decrypt_file(&mut file, &encryption_key),
             }
 
             println!("{}::{} OK", action, dir_entry.file_name().into_string().unwrap());
