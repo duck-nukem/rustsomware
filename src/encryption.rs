47492a4a4d49326f60f550019d4d9fdadefb3474
Alex Szabó <kreatemore@gmail.com>
2021-01-05 16:00:06 +0100

    Adds integration tests for encrypt/decrypt

--- src/encryption.rs
+++ src/encryption.rs
@@ -24,3 +24,54 @@ pub fn decrypt_file(encrypted_file: &mut File, key: &String) {
     encrypted_file.seek(SeekFrom::Start(0)).unwrap();
     encrypted_file.write_all(original_content.as_slice()).unwrap();
 }
+
+#[cfg(test)]
+mod tests {
+    use std::fs::{File, OpenOptions};
+    use std::io::{Write, Read};
+    use crate::encryption::{encrypt_file, decrypt_file};
+
+    #[test]
+    fn encrypts_file() {
+        let key = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+        let mut file = File::create("encrypt.txt").unwrap();
+        let original_content = "hello";
+        file.write_all(original_content.as_bytes()).unwrap();
+        file = open_file("encrypt.txt");
+
+        encrypt_file(&mut file, &key.to_owned());
+
+        let mut contents = String::new();
+        file = open_file("encrypt.txt");
+
+        // Expect to get errors reading as the encrypted file won't be valid UTF-8
+        match file.read_to_string(&mut contents) {
+            Ok(_) => assert!(false),
+            Err(_) => assert!(true),
+        }
+    }
+
+    fn open_file(filename: &str) -> File {
+        return OpenOptions::new().read(true).write(true).open(filename).unwrap();
+    }
+
+    #[test]
+    fn decrypts_file() {
+        let key = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+        let mut file = File::create("encrypt.txt").unwrap();
+        let original_content = "hello";
+        file.write_all(original_content.as_bytes()).unwrap();
+
+        file = open_file("encrypt.txt");
+        encrypt_file(&mut file, &key.to_owned());
+
+        file = open_file("encrypt.txt");
+        decrypt_file(&mut file, &key.to_owned());
+
+        let mut contents = String::new();
+        file = open_file("encrypt.txt");
+        file.read_to_string(&mut contents).unwrap();
+
+        assert_eq!(contents, original_content);
+    }
+}
